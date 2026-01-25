658ee99a325ba35ca15083a8e15b79ab03c1cb08
IfeelLikeSnow <ifeellikesnow@googlemail.com>
2026-01-25 17:47:19 +0100

    Cleanup: remove obsolete template wrapper + merge log

--- TrackTemplates/IFLSWB_Fieldrec_Slice_RS5k_Rack_Template_Package.lua
+++
@@ -1,9 +0,0 @@
--- @description IFLSWB RS5k Rack Track Template
--- @version 1.0
--- @author Ifeel
--- @about Provides RS5k rack track template for slicing workflows
--- @provides
---   [main] TrackTemplates/IFLSWB_Fieldrec_Slice_RS5k_Rack_Template.RTrackTemplate
-
--- This file only exists to package the track template properly via ReaPack.
-return
